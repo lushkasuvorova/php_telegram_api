0c683ff21221deae85b66ad9ff71ab65e6bec280
lushkasuvorova <lushkasuvorova0@gmail.com>
2023-12-17 00:02:56 +0300

    sendMessages

--- index.php
+++ index.php
@@ -7,23 +7,38 @@ class Telebot
 {
     function getUpdates()
     {
-        $url = "https://api.telegram.org/bot".$_ENV['BOT_TOKEN']."/getUpdates";
+        $url = "https://api.telegram.org/bot" . $_ENV['BOT_TOKEN'] . "/getUpdates";
         $data = json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
         $i = 0;
         foreach ($data["result"] as $message) {
-            $messages[$i][] = $message["update_id"];
-            $messages[$i][] = $message["message"]["message_id"];
-            $messages[$i][] = $message["message"]["chat"]["id"];
-            $messages[$i][] = $message["message"]["text"];
+            $messages[$i]["update_id"] = $message["update_id"];
+            $messages[$i]["message_id"] = $message["message"]["message_id"];
+            $messages[$i]["chat_id"] = $message["message"]["chat"]["id"];
+            $messages[$i]["text"] = $message["message"]["text"];
             $i++;
         }
         //print_r($messages);
         return $messages;
     }
-    function sendMessage(){
-
+    function sendMessage()
+    {
+        $data = [
+            "chat_id" => 1140339250,
+            "text" => "Test"
+        ];
+        $ch = curl_init("https://api.telegram.org/bot" . $_ENV['BOT_TOKEN'] . "/sendMessage");
+        curl_setopt_array($ch, [
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => http_build_query($data),
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 10
+        ]);
+        $result = json_decode(curl_exec($ch), true);
+        curl_close($ch);
+        //print_r($result);
     }
 }
-$Omar=new Telebot;
-$messages=$Omar->getUpdates();
-
+$Omar = new Telebot;
+$messages = $Omar->getUpdates();
+$Omar->sendMessage();
